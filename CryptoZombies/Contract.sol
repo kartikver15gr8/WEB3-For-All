cc516688bd191d009736c200748cc2d1a7fc910a
Shipra Kushwaha <92146344+Shipkush@users.noreply.github.com>
2023-02-02 22:52:47 +0530

    Create Contract.sol
    Changed unit to uint256.

--- CryptoZombies/Contract.sol
+++ CryptoZombies/Contract.sol
@@ -2,14 +2,14 @@ pragma solidity >=0.5.0 <0.6.0;
 
 contract ZombieFactory {
 
-    event NewZombie(uint zombieId, string name, uint dna);
+    event NewZombie(uint256 zombieId, string name, uint256 dna);
 
-    uint dnaDigits = 16;
-    uint dnaModulus = 10 ** dnaDigits;
+    uint256 dnaDigits = 16;
+    uint256 dnaModulus = 10 ** dnaDigits;
 
     struct Zombie {
         string name;
-        uint dna;
+         uint256  dna;
     }
 
     Zombie[] public zombies;
