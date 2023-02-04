3bc20be95fbbbc200268939ed7f935fcd72f1402
Soumya Kushwaha <kushsoumya30@gmail.com>
2023-02-04 18:57:21 +0530

    Update Contract.sol

--- CryptoZombies/Contract.sol
+++ CryptoZombies/Contract.sol
@@ -15,7 +15,7 @@ contract ZombieFactory {
     Zombie[] public zombies;
 
     mapping (uint256 => address) public zombieToOwner;
-    mapping (address => uint256) ownerZombieCount;
+    mapping (address => uint256) public ownerZombieCount;
 
     function _createZombie(string memory _name, uint _dna) private {
         uint id = zombies.push(Zombie(_name, _dna)) - 1;
